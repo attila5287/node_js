6b04b4906cd9cf3a5f79e3a72a354c52d80b9c9a
attila5287 <atiturkoz@hotmail.com>
2021-02-28 10:33:53 -0700

    last push before rename repo

--- async_await_solution/generateHTML.js
+++ async_await_solution/generateHTML.js
@@ -1,32 +1,32 @@
 const colors = {
-  green: {
-    wrapperBackground: "#E6E1C3",
-    headerBackground: "#C1C72C",
-    headerColor: "black",
-    photoBorderColor: "#black"
-  },
-  blue: {
-    wrapperBackground: "#5F64D3",
-    headerBackground: "#26175A",
-    headerColor: "white",
-    photoBorderColor: "#73448C"
-  },
-  pink: {
-    wrapperBackground: "#879CDF",
-    headerBackground: "#FF8374",
-    headerColor: "white",
-    photoBorderColor: "#FEE24C"
-  },
-  red: {
-    wrapperBackground: "#DE9967",
-    headerBackground: "#870603",
-    headerColor: "white",
-    photoBorderColor: "white"
-  }
+	green: {
+		wrapperBackground: "#E6E1C3",
+		headerBackground: "#C1C72C",
+		headerColor: "black",
+		photoBorderColor: "#black"
+	},
+	blue: {
+		wrapperBackground: "#5F64D3",
+		headerBackground: "#26175A",
+		headerColor: "white",
+		photoBorderColor: "#73448C"
+	},
+	pink: {
+		wrapperBackground: "#879CDF",
+		headerBackground: "#FF8374",
+		headerColor: "white",
+		photoBorderColor: "#FEE24C"
+	},
+	red: {
+		wrapperBackground: "#DE9967",
+		headerBackground: "#870603",
+		headerColor: "white",
+		photoBorderColor: "white"
+	}
 };
 
 function generateHTML(data) {
-  return `<!DOCTYPE html>
+	return `<!DOCTYPE html>
 <html lang="en">
    <head>
       <meta charset="UTF-8" />
@@ -170,9 +170,72 @@ function generateHTML(data) {
             zoom: .75; 
           } 
          }
-      </style>`
-        }
+      </style>
+   </head>
+   <body>
+      <div class="wrapper">
+         <div class="photo-header">
+            <img src="${data.avatar_url}" alt="Photo of ${data.name}" />
+            <h1>Hi!</h1>
+            <h2>
+            My name is ${data.name}!</h1>
+            <h5>${data.company ? `Currently @ ${data.company}` : ""}</h5>
+            <nav class="links-nav">
+               ${
+									data.location
+										? `<a class="nav-link" target="_blank" rel="noopener noreferrer" href="https://www.google.com/maps/place/${data.location}"><i class="fas fa-location-arrow"></i> ${data.location}</a>`
+										: ""
+								}
+               <a class="nav-link" target="_blank" rel="noopener noreferrer" href="${
+									data.html_url
+								}"><i class="fab fa-github-alt"></i> GitHub</a>
+               ${
+									data.blog
+										? `<a class="nav-link" target="_blank" rel="noopener noreferrer" href="${data.blog}"><i class="fas fa-rss"></i> Blog</a>`
+										: ""
+								}
+            </nav>
+         </div>
+         <main>
+            <div class="container">
+            <div class="row">
+               <div class="col">
+                  <h3>${data.bio ? `${data.bio}` : ""}</h3>
+               </div>
+               </div>
+               <div class="row">
+               <div class="col">
+                  <div class="card">
+                    <h3>Public Repositories</h3>
+                    <h4>${data.public_repos}</h4>
+                  </div>
+               </div>
+                <div class="col">
+                <div class="card">
+                  <h3>Followers</h3>
+                  <h4>${data.followers}</h4>
+                </div>
+               </div>
+               </div>
+               <div class="row">
+               <div class="col">
+               <div class="card">
+                  <h3>GitHub Stars</h3>
+                  <h4>${data.stars}</h4>
+                  </div>
+               </div>
+                <div class="col">
+                <div class="card">
+                  <h3>Following</h3>
+                  <h4>${data.following}</h4>
+                  </div>
+               </div>
+               </div>
+            </div>
+         </main>
+      </div>
+   </body>
+</html>`;
+}
 
-module.exports = {
-  generateHTML
-};
+module.exports = generateHTML;
--- async_await_solution/index.js
+++ async_await_solution/index.js
@@ -1,15 +1,63 @@
-const generateHTML = require('./generateHTML');
-// console.log('test import gen-html module');
+const axios = require( 'axios' );
+const generateHTML = require( './generateHTML' );
 const inquirer = require("inquirer");
-const questions = [
+
+
+async function init() {
+  const questions = [
+    {
+      type: "input",
+      name: "name",
+      message: "type github username..."
+    },
+    {
+      type: "list",
+      name: "color",
+      message: "choose a color to style pdf...",
+      choices: ["green", "blue", "pink", "red", ]
+    }
+  ];
+  const answers = await inquirer.prompt( questions );
+  // console.log( 'answers :>> ', answers );
   
-];
+  function prep_answers (ans) {
+    if (ans.name == "") {
+			answers.name = "attila5287";
+			console.log("name default :>> ", ans);
+		}
+    return ans;
+  }
+  const final_answers = prep_answers(answers);
+  
+  // github API
+  const url = `https://api.github.com/users/${final_answers.name}?client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}`;
 
-function writeToFile(fileName, data) {
- 
-}
+  const response = await axios.get( url );
+  // console.log( 'github response :>> ', response );
 
-function init() {
+  function prep_data ( ans, res ) {
+    
+      console.log( "res.data :>> ", res.data );
+		let d = {
+			name: "",
+			color: "",
+			data: res.data
+    };
+    
+		d.name = ans.name;
+		d.color = ans.color;
+
+		if (d.name == "") {
+			d.name = "attila5287";
+			// console.log("name default :>> ", ans);
+		}
+		return d;
+	}
+  const data = await prep_data( answers, response );
+  
+  // console.log('genHTML :>> ', generateHTML(data));
+  
+
 }
 
 init();
