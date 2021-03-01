ce9e3a39744b7701965c126d195a21fe083902b8
attila5287 <atiturkoz@hotmail.com>
2021-03-01 04:27:04 -0700

    md gen write file

--- markdown_gen/index.js
+++ markdown_gen/index.js
@@ -1,16 +1,17 @@
 // TODO: Include packages needed for this application
-
 const inquirer = require(`inquirer`);
 const fs = require("fs");
 const util = require("util");
-const writeFileAsync = util.promisify(fs.writeFile);
 
 // TODO: Create an array of questions for user input
 const questions = () => {
 };
 
 // TODO: Create a function to write README file
-const writeToFile = (data) =>`test md`;
+const writeToFile = (data) => `
+# ${data.title}
+test md
+`;
 
 // TODO: Create a function to initialize app
 const init = () => {
